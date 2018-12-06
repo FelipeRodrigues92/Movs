49403ede265c550fbdbcfdfaf6e9563b3ead757e
Felipe Rodrigues Silva <liipe.frs@gmail.com>
2018-12-05 23:19:19 -0200

    add pods and gitignore

---
+++ Movs/Movs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movs
+//
+//  Created by Felipe Rodrigues Silva on 05/12/18.
+//  Copyright © 2018 Felipe Rodrigues Silva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
